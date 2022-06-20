5a13e947a883035d51b5484d8f53f37a467b4f4e
DudzinDzmitry <dmitiy.i.dudin@gmail.com>
2022-06-20 21:43:54 +0300

    Made a mock up of linking the min_max and average threads

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <windows.h>
 
+struct MetaArray {
+    int *array;
+    int length;
+    int min;
+    int max;
+    int average;
+
+    MetaArray(int *array, int length) : array(array), length(length), min(array[0]), max(array[0]), average(array[0]) {}
+};
+
+DWORD WINAPI Min_Max(LPVOID lpParameter) {}
+
+DWORD WINAPI Average(LPVOID lpParam) {}
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     std::cout
@@ -17,6 +31,20 @@ int main() {
         std::cin >> array[i];
         ++i;
     }
+
+    MetaArray *metaArray;
+    metaArray = new MetaArray(array, length);
+
+    HANDLE min_max = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) Min_Max, LPVOID(metaArray), 0, NULL);
+    if (min_max == NULL) return GetLastError();
+    WaitForSingleObject(min_max, INFINITE);
+    CloseHandle(min_max);
+
+    HANDLE average = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) Average, LPVOID(metaArray), 0, NULL);
+    if (average == NULL) return GetLastError();
+    WaitForSingleObject(average, INFINITE);
+    CloseHandle(average);
+
     system("pause");
     return 0;
 }
